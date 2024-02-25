031fb5e1735a2947ff22e03f4be985726178892c
AhmedMeligy <ahmed.tarek540540@gmail.com>
2024-02-25 02:29:49 +0200

    give padding to the navbar

--- resources/views/components/instagram-navbar.blade.php
+++ resources/views/components/instagram-navbar.blade.php
@@ -1,5 +1,5 @@
         @section('navbar')
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-black border-end border-secondary">
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-black border-end border-secondary pt-5">
                 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                     <a href="/"
                         class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
