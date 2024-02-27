320b661e6bb639c140f63f16b8714f5e6c5ac9cb
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2024-02-27 14:53:40 +0200

    handle update user's data without reset password every time

--- resources/views/user/viewprofile.blade.php
+++ resources/views/user/viewprofile.blade.php
@@ -135,11 +135,9 @@
                                     </div>
                                 </div>
 
-                                <!-- <h4>{{ __('Change Password') }}</h4> -->
-
     <div class="col-md-6 w-75 mt-2">
         <label for="current_password" class="form-label">{{ __('Current Password') }}</label>
-        <input id="current_password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" required>
+        <input id="current_password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password">
 
         @error('current_password')
             <span class="invalid-feedback" role="alert">
@@ -150,7 +148,7 @@
 
     <div class="col-md-6 w-75">
         <label for="new_password" class="form-label">{{ __('New Password') }}</label>
-        <input id="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" required>
+        <input id="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password">
 
         @error('new_password')
             <span class="invalid-feedback" role="alert">
